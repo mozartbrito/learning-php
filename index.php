e0eaeec5b4b3d8345625f973702b17d938eb333e
Professor <professor@edu.senac.br>
2018-11-14 22:05:20 -0200

    Variables

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+		<?php 
+			$nome = "Geovane";
+			$nascimento = '10/02/1998';
+			$sobre_nome = ' Fonseca da Silva';
+			/*echo $nome; 
+			echo '<br />';
+			echo $nascimento;
+			echo '<br />';
+			echo $sobre_nome;*/
+			//esse é um comentário
+			/*
+			esse comentário é de 
+			várias linhas de 
+			texto e pode aumentar
+			*/
+			#outro tipo de comentário
+
+		?>
+		<p><?php echo $nome; ?></p>
+		<p><?php echo $sobre_nome ?></p>
+		<p><?php echo $nascimento ?></p>
+</body>
+</html>
