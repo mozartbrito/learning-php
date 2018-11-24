d16b7a709341790b74bb3cfdfb02eec7a256efbe
Professor <professor@edu.senac.br>
2018-11-23 22:02:33 -0200

    add connection

--- sistema-loja/produtos.php
+++ sistema-loja/produtos.php
@@ -1,3 +1,16 @@
+<?php 
+	//$mysqli = new mysqli("localhost", "usuario", "senha", "database");
+	//nesta linha realizo a conexão
+	$conexao = new mysqli("localhost", "root", "senac", "loja");
+	//nesta linha, monto a consulta a ser realizada
+	$sql_produtos = "SELECT p.*, c.descricao as categoria 
+					 FROM produto p
+					 LEFT JOIN  categoria c ON c.id = p.id_categoria
+					 ORDER BY p.id";
+	//nesta linha, executo a consulta montada
+	$produtos = $conexao->query($sql_produtos);
+?>
+
 <?php include "layout/header.php"; ?>
 <?php include "layout/menu.php"; ?>
 <div class="container">
@@ -23,38 +36,19 @@
 				<th>Categoria</th>
 				<th>Ações</th>
 			</tr>
-			<tr>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-			</tr>
-			<tr>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-			</tr>
-			<tr>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-			</tr>
-			<tr>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-				<td>Content</td>
-			</tr> 
+
+			<?php while($produto = $produtos->fetch_array(MYSQLI_ASSOC)) { //aqui eu starto o loop dos dados da consulta ?>
+				<tr>
+					<td><?php echo $produto['id']; ?></td>
+					<td><?php echo $produto['nome']; ?></td>
+					<td>R$ <?php echo number_format($produto['valor'], 2, ',','.'); ?></td>
+					<td><?php echo $produto['estoque']; ?></td>
+					<td><?php echo $produto['categoria'] ?></td>
+					<td></td>
+				</tr>
+
+			<?php } //aqui finalizo o loop dos dados ?>
+
 		</table>
 		
 		
