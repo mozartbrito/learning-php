b171307b2fa03ff213b5f57f55d0d7b41a6d60d5
Professor <professor@edu.senac.br>
2018-12-06 20:34:12 -0200

    usuarios

---
+++ sistema-loja/novo-usuario.php
@@ -0,0 +1,64 @@
+<?php 
+include "layout/header.php";
+include "layout/menu.php";
+
+require "includes/connection.php";
+
+$title = "Novo usuário";
+
+if(isset($_GET['id']) && $_GET['id'] != '') {
+	$id = $_GET['id'];
+	$sql_usuario = "SELECT * FROM usuario WHERE id = {$id}";
+	$dados_usuario = $conexao->query($sql_usuario)->fetch_assoc();
+
+	$title = "Editar usuário";
+}
+?>
+
+
+<div class="container">
+	<p>&nbsp;</p>
+	<h1><?php echo $title; ?></h1>
+	<div class="row">
+		<div class="col">
+			<nav aria-label="breadcrumb">
+			  <ol class="breadcrumb">
+			    <li class="breadcrumb-item"><a href="principal.php">Principal</a></li>
+			    <li class="breadcrumb-item"><a href="usuarios.php">Usuários</a></li>
+			    <li class="breadcrumb-item active" aria-current="page"><?php echo $title; ?></li>
+			  </ol>
+			</nav>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-3"></div>		
+		<div class="col-6">
+			
+			<form method="post" action="salvar-usuario.php">
+				<div class="form-group">
+					<label for="nome">Nome:</label>
+					<input type="text" name="nome" id="nome" class="form-control" required  value="<?php echo(isset($dados_usuario) ? $dados_usuario['nome'] : ''); ?>">
+					<input type="hidden" name="id" value="<?php echo(isset($dados_usuario) ? $dados_usuario['id'] : ''); ?>">
+				</div>
+				<div class="form-group">
+					<label for="email">E-mail:</label>
+					<input type="email" name="email" id="email" class="form-control" required  value="<?php echo(isset($dados_usuario) ? $dados_usuario['email'] : ''); ?>">
+				</div>
+				<div class="form-group">
+					<label for="senha">Senha:</label>
+					<input type="password" name="senha" id="senha" class="form-control" >
+				</div>
+				<button class="btn btn-primary float-right" type="submit">Salvar</button>
+			</form>
+
+
+		</div>		
+		<div class="col-3"></div>		
+		
+	</div>
+
+</div>
+<?php 
+include "layout/footer.php";
+?>
---
+++ sistema-loja/usuarios.php
@@ -0,0 +1,67 @@
+<?php 
+include "layout/header.php";
+include "layout/menu.php";
+
+require "includes/connection.php";
+
+$sql_usarios = "SELECT * FROM usuario";
+$usuarios = $conexao->query($sql_usarios);
+
+?>
+
+<div class="container">
+	<p>&nbsp;</p>
+	<h1>Usuários</h1>
+	<div class="row">
+		<div class="col">
+			<nav aria-label="breadcrumb">
+			  <ol class="breadcrumb">
+			    <li class="breadcrumb-item"><a href="principal.php">Principal</a></li>
+			    <li class="breadcrumb-item active" aria-current="page">Usuários</li>
+			  </ol>
+			</nav>
+		</div>
+	</div>
+
+	<div class="row">
+		
+		<div class="">
+			<a href="novo-usuario.php" class="btn btn-primary">
+				Novo usuário
+			</a>
+		</div>
+		<p>&nbsp;</p>
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nome</th>
+					<th>E-mail</th>
+					<th>Ações</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php while($usuario = $usuarios->fetch_array(MYSQLI_ASSOC)) { ?>
+					<tr>
+						<td><?php echo $usuario['id']; ?></td>
+						<td><?php echo $usuario['nome']; ?></td>
+						<td><?php echo $usuario['email'] ?></td>
+						<td>
+							<a href="editar-usuario.php?id=<?php echo $usuario['id']; ?>" class="btn btn-warning">
+								<i class="fas fa-edit"></i>
+							</a>
+
+							<a href="exclui-usuario.php?id=<?php echo $usuario['id']; ?>" class="btn btn-danger" onclick="return confirm('Deseja realmente excluir?')">
+								<i class="fas fa-trash"></i>
+							</a>
+						</td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+
+</div>
+<?php 
+include "layout/footer.php";
+?>
