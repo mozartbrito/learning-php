461359d5bf11609e68981bf9066a0230c4a4edb4
Professor <professor@edu.senac.br>
2018-12-14 21:00:50 -0200

    inserindo busca

--- sistema-loja/categorias.php
+++ sistema-loja/categorias.php
@@ -1,16 +1,35 @@
 <?php
 //abaixo incluo o arquivo de conexão com o banco de dados
 	require "includes/connection.php";
-
 	$sql_categorias = "SELECT * FROM categoria;";
-	$categorias = $conexao->query($sql_categorias);
 
+	if(isset($_GET['pesquisa'])) {
+		$busca = $_GET['pesquisa'];
+		$sql_categorias = "SELECT * FROM categoria
+							WHERE descricao LIKE '%{$busca}%'";
+	}
+	
+	$categorias = $conexao->query($sql_categorias);
 
 	include "layout/header.php"; 
 	include "layout/menu.php"; 
 ?>
+
 <div class="container">
+	
 	<p>&nbsp;</p>
+	<form class="form-inline">
+	  <div class="form-group mx-sm-3 mb-2">
+	    <label for="inputPassword2" class="sr-only">Pesquisa</label>
+	    <input type="text" class="form-control" id="inputPassword2" name="pesquisa" placeholder="Pesquisa">
+	  </div>
+	  <button type="submit" class="btn btn-info mb-2">
+	  	<i class="fas fa-search"></i>
+	  </button>
+	  <a href="categorias.php" class="btn btn-warning mb-2" title="Limpar">
+	  	<i class="fas fa-times-circle"></i>
+	  </a>
+	</form>
 	<h1>Categorias</h1>
 	<div class="row">
 		<div class="col">
@@ -58,6 +77,11 @@
 			<?php } ?>
 			<!-- fim da linha de loop -->
 		</table>
+		<?php 
+			if($categorias->num_rows < 1) {
+				echo '<div class="alert alert-info col-12 text-center">Não foram encontrados registros</div>';
+			}
+		?>
 	</div>
 </div>
 
--- sistema-loja/includes/connection.php
+++ sistema-loja/includes/connection.php
@@ -1,3 +1,8 @@
 <?php 
+//localhost: host ou domínio da conexão
+//root: usuário do banco de dados
+//senac: senha do banco de dados
+//loja: banco de dados a ser conectado
+
 	$conexao = new mysqli("localhost", "root", "senac", "loja");
 ?>
